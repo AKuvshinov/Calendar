ac0fc38bf2cb6fec986374990fa61af39efa1e7f
AleksKuwshinov <kuv_aleks@mail.ru>
2021-02-18 13:07:44 +0400

    Выполнил домашнее задание

--- app/src/main/java/com/aleksey_kuvshinov/calendar/MainActivity.java
+++ app/src/main/java/com/aleksey_kuvshinov/calendar/MainActivity.java
@@ -2,13 +2,25 @@ package com.aleksey_kuvshinov.calendar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button2 = (Button)findViewById(R.id.button2);
+        button2.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i;
+        i = new Intent(this, Calendar2.class);
+        startActivity(i);
     }
 }
